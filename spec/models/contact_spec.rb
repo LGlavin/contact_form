a896102e263881854a723640c8efaf95365ff33f
Elizabeth Glavin <eglavin@gmail.com>
2013-09-17 16:19:23 -0400

    finished for now

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -6,4 +6,13 @@ describe Contact do
 
  it { should have_valid(:last_name).when('glavin') }
  it { should_not have_valid(:last_name).when(nil,'')}
+
+ it { should have_valid(:email).when('eglavin@gmail.com') }
+ it { should_not have_valid(:email).when(nil,'') }
+
+ it { should have_vaild(:subject).when('awesome') }
+ it { should_not have_valid(:subject).when(nil,'') }
+
+ it { should have_valid(:description).when('Tell us why we are awesome!')}
+ it { should_not have_valid(:description).when(nil,'')}
 end
